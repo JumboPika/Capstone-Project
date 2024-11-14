759fa072be3fa0afb6bb03bfd873a29a235f99e4
JumboPika <111240337+JumboPika@users.noreply.github.com>
2024-11-14 17:57:18 +0800

    Update and rename test_send_oldold.py to test_send_nosave.py

--- test_send_nosave.py
+++ test_send_nosave.py
@@ -1,11 +1,9 @@
 import sys
-import os
 import numpy as np
 import cv2 as cv
-import time
 import socket
+import time
 from mp_pose import MPPose
-print(cv.getBuildInformation())
 
 sys.path.append('../person_detection_mediapipe')
 from mp_persondet import MPPersonDet
@@ -13,38 +11,6 @@ from mp_persondet import MPPersonDet
 # 設定多播參數
 MULTICAST_GROUP = '224.1.1.1'  # 任選私有多播地址
 MULTICAST_PORT = 5005
-
-# 設定錄影參數
-#fourcc = cv.VideoWriter_fourcc(*'XVID')
-fourcc = cv.VideoWriter_fourcc(*'MJPG')
-#fourcc = cv.VideoWriter_fourcc(*'H264')
-video_dir = './example_outputs'
-
-
-recording = False
-video_writer = None
-video_filename = None
-
-def start_recording(frame):
-    global recording, video_writer, video_filename
-    timestamp = int(time.time())
-    #video_filename = f"{video_dir}/video_{timestamp}.avi"
-    video_filename = f"{video_dir}/video_{timestamp}.mp4"
-    video_writer = cv.VideoWriter(video_filename, fourcc, 20.0, (frame.shape[1], frame.shape[0]))
-    recording = True
-    print("開始錄影")
-
-def stop_recording(save):
-    global recording, video_writer, video_filename
-    if recording:
-        video_writer.release()
-        if not save and video_filename:
-            os.remove(video_filename)
-            print("錄影已停止，影片已捨棄")
-        elif save:
-            print("錄影已停止，影片已保存")
-        recording = False
-        video_filename = None
 
 def is_lying_down(landmarks):
     return landmarks[0][1] > landmarks[23][1] + 20
@@ -76,23 +42,18 @@ if __name__ == '__main__':
                 pose = pose_estimator.infer(frame, person)
                 if pose:
                     _, landmarks_screen, _, _, _, _ = pose
+                    movement = None
 
                     if is_turning_waist(landmarks_screen):
-                        if recording:
-                            stop_recording(save=True)
                         movement = 'turning_waist'
-
                     elif is_sitting_up(landmarks_screen):
-                        if not recording:
-                            start_recording(frame)
                         movement = 'sitting_up'
-
                     elif is_lying_down(landmarks_screen):
-                        if recording:
-                            stop_recording(save=False)
                         movement = 'lying_down'
 
-                    sock.sendto(movement.encode(), (MULTICAST_GROUP, MULTICAST_PORT))
-                    print(f"Current Movement: {movement}")
+                    if movement:
+                        sock.sendto(movement.encode(), (MULTICAST_GROUP, MULTICAST_PORT))
+                        print(f"Current Movement: {movement}")
+
     cap.release()
     cv.destroyAllWindows()
